dc9f0eecf0db9f5b891fa940e53f79efd7db364f
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-05-06 20:42:07 +0200

    Init git

---
+++ Slack_Api_Practice/Slack_Api_Practice/Controllers/SlackController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Slack_Api_Practice.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace Slack_Api_Practice.Controllers
+{
+
+    public class SlackController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public SlackController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        [HttpPost("slack")]
+        public IActionResult SendEmail([FromForm] string email, [FromForm] string messageToSend )
+        {
+            var Client = new HttpClient();
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", configuration["Token"]);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://slack.com/api/users.lookupByEmail");
+
+            var list = new List<KeyValuePair<string, string>>();
+            list.Add(new KeyValuePair<string, string>("email", email));
+  
+            request.Content = new FormUrlEncodedContent(list);
+
+            var response = Client.SendAsync(request).Result;
+            EmailLookupResponse responseObject = new EmailLookupResponse();
+            responseObject = JsonConvert.DeserializeObject<EmailLookupResponse>(response.Content.ReadAsStringAsync().Result);
+            string responseString = response.Content.ReadAsStringAsync().Result;
+
+            var postMessageRequest = new HttpRequestMessage(HttpMethod.Post, "https://slack.com/api/chat.postMessage");
+
+            var messageRequestBody = new List<KeyValuePair<string, string>>();
+            messageRequestBody.Add(new KeyValuePair<string, string>("channel", responseObject.user.id));
+            messageRequestBody.Add(new KeyValuePair<string, string>("text", messageToSend));
+            postMessageRequest.Content = new FormUrlEncodedContent(messageRequestBody);
+
+            Client.SendAsync(postMessageRequest);
+
+            return Ok(new {messageSentTo = email, userId = responseObject.user.id });
+        }
+    }
+}
---
+++ Slack_Api_Practice/Slack_Api_Practice/Models/EmailLookupResponse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slack_Api_Practice.Models
+{
+
+    public class EmailLookupResponse
+    {
+        public bool ok { get; set; }
+        public User user { get; set; }
+    }
+
+    public class User
+    {
+        public string id { get; set; }
+        public string team_id { get; set; }
+        public string name { get; set; }
+        public bool deleted { get; set; }
+        public string color { get; set; }
+        public string real_name { get; set; }
+        public string tz { get; set; }
+        public string tz_label { get; set; }
+        public int tz_offset { get; set; }
+        public Profile profile { get; set; }
+        public bool is_admin { get; set; }
+        public bool is_owner { get; set; }
+        public bool is_primary_owner { get; set; }
+        public bool is_restricted { get; set; }
+        public bool is_ultra_restricted { get; set; }
+        public bool is_bot { get; set; }
+        public bool is_app_user { get; set; }
+        public int updated { get; set; }
+        public bool has_2fa { get; set; }
+    }
+
+    public class Profile
+    {
+        public string title { get; set; }
+        public string phone { get; set; }
+        public string skype { get; set; }
+        public string real_name { get; set; }
+        public string real_name_normalized { get; set; }
+        public string display_name { get; set; }
+        public string display_name_normalized { get; set; }
+        public string status_text { get; set; }
+        public string status_emoji { get; set; }
+        public int status_expiration { get; set; }
+        public string avatar_hash { get; set; }
+        public string email { get; set; }
+        public string image_24 { get; set; }
+        public string image_32 { get; set; }
+        public string image_48 { get; set; }
+        public string image_72 { get; set; }
+        public string image_192 { get; set; }
+        public string image_512 { get; set; }
+        public string status_text_canonical { get; set; }
+        public string team { get; set; }
+    }
+
+}
